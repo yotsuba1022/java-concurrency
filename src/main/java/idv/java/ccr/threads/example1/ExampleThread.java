21e504ae631ab0b64cc527df83bc8222d5a94ded
yotsuba1022 <yotsuba1022@gmail.com>
2017-08-30 00:10:58 +0800

    Polish.

--- src/main/java/idv/java/ccr/threads/example1/ExampleThread.java
+++ src/main/java/idv/java/ccr/threads/example1/ExampleThread.java
@@ -8,21 +8,27 @@ import idv.java.ccr.util.ThreadColor;
 public class ExampleThread {
 
     public static void main(String[] args) {
+        String mainThreadConsoleColor = ThreadColor.ANSI_YELLOW;
+        String thread1ConsoleColor = ThreadColor.ANSI_CYAN;
+        String thread2ConsoleColor = ThreadColor.ANSI_MAGENTA;
+
+        System.out.println(mainThreadConsoleColor + "Available processors: " + Runtime.getRuntime().availableProcessors());
+
         Thread thread1 = new Thread(() -> {
-            System.out.println(ThreadColor.ANSI_CYAN + "In thread1");
-            showThreadStatus(Thread.currentThread());
+            System.out.println(thread1ConsoleColor + "In thread1");
+            showThreadStatus(Thread.currentThread(), thread1ConsoleColor);
         }, "Thread1");
 
         Thread thread2 = new Thread(() -> {
-            System.out.println(ThreadColor.ANSI_MAGENTA + "In thread2");
-            showThreadStatus(Thread.currentThread());
+            System.out.println(thread2ConsoleColor + "In thread2");
+            showThreadStatus(Thread.currentThread(), thread2ConsoleColor);
         }, "Thread2");
 
-        System.out.println(ThreadColor.ANSI_YELLOW + "Before execution:");
-        showThreadStatus(thread1);
-        showThreadStatus(thread2);
+        System.out.println(mainThreadConsoleColor + "Before execution:");
+        showThreadStatus(thread1, thread1ConsoleColor);
+        showThreadStatus(thread2, thread2ConsoleColor);
 
-        System.out.println(ThreadColor.ANSI_GREEN + "Start execution:");
+        System.out.println(mainThreadConsoleColor + "Start execution:");
         thread1.start();
         thread2.start();
 
@@ -36,14 +42,15 @@ public class ExampleThread {
         } catch (InterruptedException ignored) {
         }
 
-        System.out.println(ThreadColor.ANSI_BLUE + "After execution:");
-        showThreadStatus(thread1);
-        showThreadStatus(thread2);
+        System.out.println(mainThreadConsoleColor + "After execution:");
+        showThreadStatus(thread1, thread1ConsoleColor);
+        showThreadStatus(thread2, thread2ConsoleColor);
     }
 
-    private static void showThreadStatus(Thread thread) {
-        System.out.println("Thread name: " + thread.getName() + ", id: " + thread.getId() + ", is alive: " + thread.isAlive()
-                + ", is daemon: " + thread.isDaemon() + ", execution state: " + thread.getState());
+    private static void showThreadStatus(Thread thread, String consoleColor) {
+        System.out.println(
+                consoleColor + "Thread name: " + thread.getName() + ", id: " + thread.getId() + ", is alive: " + thread.isAlive()
+                        + ", is daemon: " + thread.isDaemon() + ", execution state: " + thread.getState());
     }
 
 }
