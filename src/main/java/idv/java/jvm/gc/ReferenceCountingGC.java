709f24f474bf3b82c3215c998b6638151a6ca8e0
Carl <yotsuba1022@gmail.com>
2017-12-26 16:15:53 +0800

    Add ReferenceCountingGC example.

---
+++ src/main/java/idv/java/jvm/gc/ReferenceCountingGC.java
@@ -0,0 +1,27 @@
+package idv.java.jvm.gc;
+
+/**
+ * @author Carl Lu
+ * VM args: -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gclog.log
+ */
+public class ReferenceCountingGC {
+
+    private final static int _1MB = 1024 * 1024;
+    public Object instance = null;
+    private byte[] bigSize = new byte[2 * _1MB];
+
+    public static void testGC() {
+        ReferenceCountingGC objA = new ReferenceCountingGC();
+        ReferenceCountingGC objB = new ReferenceCountingGC();
+
+        objA.instance = objB;
+        objB.instance = objA;
+
+        System.gc();
+    }
+
+    public static void main(String[] args) {
+        ReferenceCountingGC.testGC();
+    }
+
+}
